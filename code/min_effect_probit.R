4f7505f8b812cb5e7cfc16ecbd4c19ca6e5ebd95
Jesper Wulff <jespernydamwulff@gmail.com>
2026-03-10 09:57:09 +0100

    Fix dist argument bug in min_effect_probit.R

--- code/min_effect_probit.R
+++ code/min_effect_probit.R
@@ -15,68 +15,56 @@ library(pwrss)
 
 # Simple example
 pwrss.z.logreg(p0 = 0.5, r2.other.x = 0.4,
-                alpha = 0.05, beta1 = -0.05087, n=9915,
-               distribution = list(dist = "normal", mean = 0, sd = 3),
-               )
+               alpha = 0.05, beta1 = -0.05087, n = 9915,
+               distribution = list(dist = "normal", mean = 0, sd = 3))
 
-# function that finds minimum effect using binary search
+# Function that finds minimum |beta1| for a given power using binary search
 find_beta1_for_power <- function(target_power = 0.5, 
                                  p0 = 0.5, 
                                  r2.other.x = 0.4,
                                  alpha = 0.05, 
                                  n = 9915,
-                                 dist = "normal",
+                                 distribution = list(dist = "normal", mean = 0, sd = 3),
+                                 direction = "negative",
                                  tolerance = 1e-4,
                                  max_iterations = 100,
-                                 beta_min = -1,
+                                 beta_min = 0.001,
                                  beta_max = 1) {
   
   library(pwrss)
   
+  sign <- if (direction == "negative") -1 else 1
   iterations <- 0
   lower <- beta_min
   upper <- beta_max
   
-  # Binary search algorithm
+  # Binary search over |beta1|
   while ((upper - lower) > tolerance && iterations < max_iterations) {
     iterations <- iterations + 1
     mid <- (lower + upper) / 2
     
-    # Calculate power at the midpoint
     current_power <- pwrss.z.logreg(p0 = p0, 
                                     r2.other.x = r2.other.x,
                                     alpha = alpha, 
-                                    beta1 = mid, 
+                                    beta1 = sign * mid, 
                                     n = n,
-                                    dist = dist,
+                                    distribution = distribution,
                                     verbose = FALSE)$power
     
-    # Adjust search boundaries based on comparison with target power
     if (current_power < target_power) {
-      # If power is too small, we need a larger effect size (more negative for negative beta1)
-      if (mid < 0) {
-        upper <- mid  # For negative beta1, make it more negative
-      } else {
-        lower <- mid  # For positive beta1, make it larger
-      }
+      lower <- mid   # Need larger |beta1| for more power
     } else {
-      # If power is too large, we need a smaller effect size
-      if (mid < 0) {
-        lower <- mid  # For negative beta1, make it less negative
-      } else {
-        upper <- mid  # For positive beta1, make it smaller
-      }
+      upper <- mid   # Can get away with smaller |beta1|
     }
   }
   
-  # Final check with the midpoint
-  final_beta <- (lower + upper) / 2
+  final_beta <- sign * (lower + upper) / 2
   final_power <- pwrss.z.logreg(p0 = p0, 
                                 r2.other.x = r2.other.x,
                                 alpha = alpha, 
                                 beta1 = final_beta, 
                                 n = n,
-                                dist = dist,
+                                distribution = distribution,
                                 verbose = FALSE)
   
   return(list(beta1 = final_beta, 
@@ -89,7 +77,8 @@ result <- find_beta1_for_power(target_power = 0.5,
                                r2.other.x = 0.01,
                                alpha = 0.05,
                                n = 9915,
-                               dist = list(dist = "normal", mean = 0, sd = 3))
+                               distribution = list(dist = "normal", mean = 0, sd = 3.8))
+result
 
 # Print results
 print(paste("Logit beta1 value for power = 0.5:", round(result$beta1, 5)))
@@ -97,5 +86,4 @@ print(paste("Probit beta1 value for power = 0.5:", round(result$beta1/1.6, 5))) 
 print(paste("Probit marginal effect value for power = 0.5:", round(result$beta1/1.6*0.4, 5))) #AME <= 0.4*beta_probit
 print(paste("Actual power achieved:", round(result$power, 5)))
 print(paste("Iterations required:", result$iterations))
-
 
